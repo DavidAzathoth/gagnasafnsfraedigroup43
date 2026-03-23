08e362cb6a144a3a54cc0c5287fcafd348457eaf
Baldvinn <baldvin.hrafn@gmail.com>
2026-03-23 15:08:39 +0000

    C update

--- DATABASE/Updated schema/DDL.sql
+++ DATABASE/Updated schema/DDL.sql
@@ -1,3 +1,26 @@
 -- Task C3
 
+CREATE TABLE raforka_legacy.fyrirtaeki_skraning (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    kennitala CHAR(10) UNIQUE NOT NULL,
+    heiti VARCHAR(100) NOT NULL,
+
+    CHECK(kennitala ~ '^[0-9]{10}$')
+);
+
+CREATE TABLE raforka_legacy.notendur_skraning (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    heiti VARCHAR(100) NOT NULL,
+    ar_stofnad integer NOT NULL,
+    "X_HNIT" double precision,
+    "Y_HNIT" double precision,
+    eigandi_id integer,
+
+    CHECK(ar_stofnad >= 1900 AND ar_stofnad <= EXTRACT(YEAR FROM CURRENT_DATE)),
+
+    FOREIGN KEY(eigandi_id) REFERENCES raforka_legacy.fyrirtaeki_skraning(id)
+    ON DELETE CASCADE
+);
+
+
 -- Task D1
