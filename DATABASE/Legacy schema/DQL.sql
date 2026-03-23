01ceec08dee147e1ccf7c227611765badfb37a3f
Baldvinn <baldvin.hrafn@gmail.com>
2026-03-23 12:09:27 +0000

    A2 update

--- DATABASE/Legacy schema/DQL.sql
+++ DATABASE/Legacy schema/DQL.sql
@@ -1,4 +1,3 @@
--- Task A2
 SELECT *
 FROM raforka_legacy.notendur_skraning
 
@@ -16,8 +15,26 @@ SELECT *
 FROM raforka_legacy.orku_maelingar
 LIMIT 20
 
-SELECT eining_heiti, EXTRACT(year FROM timi) as year, EXTRACT(month FROM timi) as month, tegund_maelingar, SUM(gildi_kwh) as total_kwh
+-- Task A2
+
+-- Querie 1
+-- Calculate the sum of kwh for each month and each type in 2025
+SELECT 
+    eining_heiti as power_plant_source,
+    EXTRACT(year FROM timi) as year,
+    EXTRACT(month FROM timi) as month,
+    tegund_maelingar as measurement_type,
+    SUM(gildi_kwh) as total_kwh
 FROM raforka_legacy.orku_maelingar
 where EXTRACT(year FROM timi) = 2025
-GROUP BY eining_heiti, EXTRACT(year FROM timi), EXTRACT(month FROM timi), tegund_maelingar, total_kwh
-LIMIT 50
+GROUP BY
+    eining_heiti,
+    EXTRACT(year FROM timi),
+    EXTRACT(month FROM timi),
+    tegund_maelingar
+ORDER BY eining_heiti, month, total_kwh DESC
+
+
+-- Querie 2
+
+
