13c7356a6a5865fbc8259fa9e206b47a6491cde8
AdamAtli <adamatlisandgreen@gmail.com>
2026-03-23 13:01:03 +0000

    Update DQL.sql

--- DATABASE/Legacy schema/DQL.sql
+++ DATABASE/Legacy schema/DQL.sql
@@ -38,3 +38,45 @@ ORDER BY eining_heiti, month, total_kwh DESC;
 -- Querie 2
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ -- Querie 3
+DROP VIEW IF EXISTS montly_power_plant_energy_view;
+
+CREATE VIEW montly_power_plant_energy_view as
+SELECT
+    eining_heiti as power_plant_source,
+    EXTRACT(YEAR FROM timi) as year,
+    EXTRACT(MONTH FROM timi) as month,
+    SUM(gildi_kwh) FILTER (WHERE tegund_maelingar = 'Framleiðsla') AS production_kwh,
+    SUM(gildi_kwh) FILTER (WHERE tegund_maelingar = 'Innmötun') AS inflow_kwh,
+    SUM(gildi_kwh) FILTER (WHERE tegund_maelingar = 'Úttekt') AS withdrawal_kwh
+FROM raforka_legacy.orku_maelingar
+WHERE EXTRACT(YEAR FROM timi) = 2025
+GROUP BY
+    eining_heiti,
+    EXTRACT(YEAR FROM timi),
+    EXTRACT(MONTH FROM timi);
+
+SELECT
+    power_plant_source,
+    AVG((production_kwh - inflow_kwh) / production_kwh) AS plant_to_sub_loss_ratio,
+    AVG((production_kwh - withdrawal_kwh) / production_kwh) AS total_system_loss_ratio
+FROM montly_power_plant_energy_view
+GROUP BY power_plant_source
+ORDER BY power_plant_source ASC;
