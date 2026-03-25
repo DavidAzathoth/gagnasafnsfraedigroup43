abc2143454d25185a03ba5dad3236edecc3a80b8
AdamAtli <adamatlisandgreen@gmail.com>
2026-03-25 15:57:26 +0000

    Update migrate_data.py

--- DATABASE/Updated schema/migrate_data.py
+++ DATABASE/Updated schema/migrate_data.py
@@ -1,1 +1,25 @@
 # Task C4
+from sqlalchemy import create_engine, text
+
+DATABASE_URL = "postgresql+psycopg2://postgres:123@localhost:5432/OrkuflaediIsland"
+
+def migrate_data():
+    engine = create_engine(DATABASE_URL)
+
+    with engine.begin() as connection:
+        
+
+        connection.execute(text("""
+            INSERT INTO raforka_updated.eigendur_notenda (kennitala, heiti)
+            SELECT DISTINCT kennitala, eigandi
+            FROM raforka_legacy.notendur_skraning
+            WHERE kennitala IS NOT NULL
+                AND eigandi IS NOT NULL
+"""))
+
+        
+
+
+
+if __name__ == "__main__":
+    migrate_data()
