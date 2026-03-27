7455077b39534c2cf406de8a3250aef10fa31ca7
Davíd Örvar Elíasson <44362281+DavidAzathoth@users.noreply.github.com>
2026-03-27 14:59:00 +0000

    reverted migrate_data

--- DATABASE/Updated schema/migrate_data.py
+++ DATABASE/Updated schema/migrate_data.py
@@ -23,9 +23,10 @@ def migrate_data():
 """))
         connection.execute(text("""
             INSERT INTO raforka_updated.orku_einingar
-            (heiti, ar_uppsett, "X_HNIT", "Y_HNIT")
+            (heiti, tegund, ar_uppsett, "X_HNIT", "Y_HNIT")
             SELECT
                 heiti,
+                tegund,
                 MAKE_DATE(ar_uppsett, manudir_uppsett, dagur_uppsett),
                 CAST("X_HNIT" AS DECIMAL(9,6)),
                 CAST("Y_HNIT" AS DECIMAL(9,6))
@@ -40,7 +41,7 @@ def migrate_data():
 """))
         connection.execute(text("""
             UPDATE raforka_updated.orku_einingar new
-            SET stod = m2.new_id
+            SET tengd_stod = m2.new_id
             FROM raforka_legacy.orku_einingar old
             JOIN id_map m1 ON old.id = m1.old_id
 
@@ -60,30 +61,8 @@ def migrate_data():
             JOIN raforka_updated.eigendur_eininga e ON e.heiti = old.eigandi 
             WHERE new.id = m.new_id;
 """))
-        connection.execute(text("""
-            INSERT INTO raforka_updated.stodvar
-            (id, tegund_stod)
-            SELECT
-                new.id,
-                old.tegund_stod
-            FROM
-            raforka_updated.orku_einingar new
-            JOIN id_map m ON m.new_id = new.id
-            JOIN raforka_legacy.orku_einingar old ON old.id = m.old_id
-            WHERE old.tegund = 'stod'
-"""))
-        connection.execute(text("""
-            INSERT INTO raforka_updated.virkjanir
-            (id, tegund_stod)
-            SELECT
-                new.id,
-                old.tegund_stod
-            FROM
-            raforka_updated.orku_einingar new
-            JOIN id_map m ON m.new_id = new.id
-            JOIN raforka_legacy.orku_einingar old ON old.id = m.old_id
-            WHERE old.tegund = 'virkjun'
-"""))
+
+
 
 
 
